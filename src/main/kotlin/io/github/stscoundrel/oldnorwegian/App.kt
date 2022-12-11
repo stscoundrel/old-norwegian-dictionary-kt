c3d5e420d59218ae7627fb8ac6f371a429dfd6a7
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-12-11 11:02:07 +0200

    Rewrite src & test scaffold in Kotlin

---
+++ src/main/kotlin/io/github/stscoundrel/oldnorwegian/App.kt
@@ -0,0 +1,9 @@
+package io.github.stscoundrel.oldnorwegian
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
+
+fun dummyMethod(first: Int, second: Int): Int {
+    return first + second
+}
