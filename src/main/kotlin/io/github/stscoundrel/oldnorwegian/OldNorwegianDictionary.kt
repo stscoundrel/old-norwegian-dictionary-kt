0ceafe5000a060a1fb23547eb69000d9241f0f49
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-11-30 19:47:41 +0200

    Dictionary entries: use lazy init instead of memo
    Closes #42

--- src/main/kotlin/io/github/stscoundrel/oldnorwegian/OldNorwegianDictionary.kt
+++ src/main/kotlin/io/github/stscoundrel/oldnorwegian/OldNorwegianDictionary.kt
@@ -5,23 +5,18 @@ import com.google.gson.Gson
 
 class OldNorwegianDictionary {
     private val DICTIONARY_RESOURCE_PATH = "/old-norwegian-dictionary.json"
-    private var entries: Array<DictionaryEntry> = arrayOf()
+    private val dictionaryEntries: Array<DictionaryEntry> by lazy { readDictionary() }
     private val gson = Gson()
 
     private fun readDictionary(): Array<DictionaryEntry> {
         val inputStream = javaClass.getResourceAsStream(DICTIONARY_RESOURCE_PATH)
         val reader = InputStreamReader(inputStream)
 
-        val result = gson.fromJson(reader, Array<DictionaryEntry>::class.java)
-
-        return result
+        return gson.fromJson(reader, Array<DictionaryEntry>::class.java)
     }
 
     fun getEntries(): Array<DictionaryEntry> {
-        if (entries.isEmpty()) {
-            entries = readDictionary()
-        }
-
-        return entries
+        // Exists mostly to preserve existing interface.
+        return dictionaryEntries
     }
 }
